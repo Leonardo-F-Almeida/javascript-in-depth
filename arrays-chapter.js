816d9eb75234a9d2f8e656c63e8c371ecf0698a8
leonardo_f.almeida <leonardo_f.almeida@hotmail.com>
2018-07-09 09:17:17 -0300

    Array length

--- arrays-chapter.js
+++ arrays-chapter.js
@@ -27,3 +27,42 @@ var numbers_object = {
 // numbers_object inherits from Object.prototype, so number inherits a larger set of
 // useful methods. Also numbers gets the mysterious length property, while numbers_object
 // does not
+
+
+// Js allows an array to contain any mixture of values
+var misc = [
+    'string',98.6,true,false,null,undefined,
+    ['nested','array'], {object:true},NaN,
+    Infinity
+]
+
+misc.length // 10
+
+
+/*
+* LENGTH
+*/
+
+var myArray = []
+myArray.length // 0
+
+
+myArray[10000] = true
+myArray.length //10001
+// myArray contains one property
+
+
+// The length can be set explicity. Making the length larger does not allocate more
+// space for the array
+
+numbers.length = 3
+// number is ['zero', 'one', 'two']
+
+// A new element can be appended to the end of an array by assigning to the array´s
+// current length
+
+number[numbers.length] = 'shi'
+// numbers is ['zero', 'one', 'two', 'shi']
+
+// It is More convenient to use the push method to accomplish the same thing:
+numbers.push('go')
